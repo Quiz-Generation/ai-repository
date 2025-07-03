8f8ad836f7b0a7680951f75747dc1c4b6f441470
junyong1111 <jypark93@kookmin.ac.kr>
2025-07-03 16:23:16 +0900

    fix :스크립투 수정3

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,12 +11,9 @@ fi
 host_ip="$2"
 
 if [ -z "$host_ip" ]; then
-    # host_ip='127.0.0.1'
-    host_ip='0.0.0.0'
+    host_ip='127.0.0.1'
 fi
 
-# package_abs_path=$(dirname $0)
-# pushd $package_abs_path
 # 절대경로
 package_abs_path=$(dirname $(dirname $(realpath "$0")))
 cd "$package_abs_path"
@@ -25,8 +22,9 @@ export PYTHONPATH=$PYTHONPATH:`pwd`
 
 . venv/bin/activate
 
-gunicorn -c gunicorn.conf.py --log-config gunicorn_log.conf --bind $host_ip:$port_number --pid ./gunicorn.pid
+
+# gunicorn -c ../gunicorn.conf.py --log-config ../gunicorn_log.conf  --bind 0.0.0.0:$port_number --log-config ../gunicorn_log.conf --pid ../gunicorn.pid
+gunicorn -c /home/jypark/ai-repository/gunicorn.conf.py --log-config /home/jypark/ai-repository/gunicorn_log.conf --bind 0.0.0.0:$port_number --pid /home/jypark/ai-repository/gunicorn.pid
 
 deactivate
 
-popd
